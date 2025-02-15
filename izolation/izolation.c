ee5ed1e68e203488e4708b12fbdc636f684ebcb6
Razvan Podar <razvan_podar@yahoo.com>
2025-02-16 00:28:27 +0200

    Add thirdparty libraries and a main file

---
+++ izolation/izolation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "stb/stb_image.h"
+
+int main(void)
+{
+    printf("iZolation!\n");
+
+    getchar();
+
+    return 0;
+}
